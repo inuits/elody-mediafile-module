861f849b9becd05f37e5313ac03751c00af6e74f
Berend Vandenbussche <berend@inuits.eu>
2026-02-20 12:05:19 +0100

    Chore: Get queries from node_modules
    Refs #153638

--- codegen.ci.ts
+++ codegen.ci.ts
@@ -7,7 +7,7 @@ const config: CodegenConfig = {
     './*.schema.ts',
     './node_modules/**/*.schema.ts',
   ],
-  documents: ['./baseModule/**/*.queries.ts', './queries/**/*.queries.ts'],
+  documents: ['./baseModule/**/*.queries.ts', './queries/**/*.queries.ts', './node_modules/**/*.queries.ts'],
   generates: {
     './generated-types/type-defs.ts': {
       plugins: [{ typescript: {} }, { 'typescript-resolvers': {} }],
