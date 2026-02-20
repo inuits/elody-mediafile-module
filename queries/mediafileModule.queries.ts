e5acaa70eff768cebe44ae03c2ef8dfb03598d9d
Berend Vandenbussche <berend@inuits.eu>
2026-02-20 12:03:49 +0100

    Chore: Move mediafile stuff
    Refs #153638

--- queries/mediafileModule.queries.ts
+++ queries/mediafileModule.queries.ts
@@ -1,6 +1,126 @@
 import { gql } from "graphql-modules";
 
 export const mediafileModuleQueries = gql`
+    query GetMediafiles(
+        $type: Entitytyping!
+        $limit: Int
+        $skip: Int
+        $searchValue: SearchFilter!
+        $advancedSearchValue: [FilterInput]
+        $advancedFilterInputs: [AdvancedFilterInput!]!
+        $searchInputType: SearchInputType
+    ) {
+        Entities(
+            type: $type
+            limit: $limit
+            skip: $skip
+            searchValue: $searchValue
+            advancedSearchValue: $advancedSearchValue
+            advancedFilterInputs: $advancedFilterInputs
+            searchInputType: $searchInputType
+        ) {
+            count
+            limit
+            results {
+                id
+                uuid
+                type
+                ... on MediaFileEntity {
+                    ...mediafileInEntity
+                }
+            }
+            __typename
+        }
+        __typename
+    }
+
+    query GetMediafilesInEntityFilters($entityType: String!) {
+        EntityTypeFilters(type: $entityType) {
+            advancedFilters {
+                type: advancedFilter(type: type) {
+                    type
+                    defaultValue(value: "mediafile")
+                    hidden(value: true)
+                }
+                technical_origin: advancedFilter(
+                    type: text
+                    key: ["elody:1|technical_origin"]
+                ) {
+                    type
+                    key
+                    hidden(value: true)
+                    defaultValue(value: "original")
+                }
+                relation: advancedFilter(
+                    type: selection
+                    parentKey: "relations"
+                    key: "hasMediafile"
+                ) {
+                    type
+                    key
+                    parentKey
+                    defaultValue(value: "")
+                    hidden(value: true)
+                }
+            }
+        }
+    }
+
+    query GetBulkOperationsForMediafiles {
+        CustomBulkOperations {
+            bulkOperationOptions {
+                options(
+                    input: [
+                        {
+                            icon: PlusCircle
+                            label: "bulk-operations.add-existing-relation"
+                            value: "addRelation"
+                            actionContext: {
+                                activeViewMode: readMode
+                                entitiesSelectionType: noneSelected
+                                labelForTooltip: "tooltip.bulkOperationsActionBar.readmode-noneselected"
+                            }
+                            bulkOperationModal: {
+                                typeModal: DynamicForm
+                                formQuery: "GetImportMediafilesQuery"
+                                askForCloseConfirmation: true
+                                neededPermission: canupdate
+                            }
+                        }
+                        {
+                            icon: DownloadAlt
+                            label: "bulk-operations.download-mediafiles"
+                            value: "downloadMediafiles"
+                            actionContext: {
+                                activeViewMode: readMode
+                                entitiesSelectionType: someSelected
+                                labelForTooltip: "tooltip.bulkOperationsActionBar.readmode-someselected"
+                            }
+                            bulkOperationModal: {
+                                typeModal: DynamicForm
+                                formQuery: "GetDownloadMediafilesForm"
+                                formRelationType: "hasMediafile"
+                                askForCloseConfirmation: true
+                                neededPermission: cancreate
+                            }
+                        }
+                    ]
+                ) {
+                    icon
+                    label
+                    value
+                    primary
+                    actionContext {
+                        ...actionContext
+                    }
+                    bulkOperationModal {
+                        ...bulkOperationModal
+                    }
+                }
+            }
+        }
+    }
+    
     mutation GenerateTranscode(
         $mediafileIds: [String!]!
         $transcodeType: TranscodeType!
