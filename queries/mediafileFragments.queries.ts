64bba8a7850361b435088d4416cb2e0b7e4c9395
Berend Vandenbussche <berend@inuits.eu>
2026-02-20 12:00:47 +0100

    Chore: Move mediafile fragment in correct module
    Refs #153638

---
+++ queries/mediafileFragments.queries.ts
@@ -0,0 +1,103 @@
+import { gql } from "graphql-modules";
+
+export const mediafileModuleQueries = gql`
+fragment mediafileInEntity on MediaFileEntity {
+...minimalBaseEntity
+    intialValues {
+        id
+        filename: keyValue(key: "filename", source: root)
+        original_filename: keyValue(key: "original_filename", source: root)
+        display_filename: keyValue(key: "display_filename", source: root)
+        original_file_location: keyValue(
+            key: "original_file_location"
+        source: root
+    )
+        transcode_file_location: keyValue(
+            key: "transcode_file_location"
+        source: derivatives
+        technicalOrigin: "transcode"
+    )
+        thumbnail: keyValue(key: "filename", source: root)
+        mimetype: keyValue(key: "mimetype", source: root)
+        height: keyValue(key: "img_height", source: root)
+        width: keyValue(key: "img_width", source: root)
+        __typename
+    }
+    teaserMetadata {
+        thumbnail: thumbnail {
+            key(input: "thumbnail")
+            __typename
+        }
+        original_filename: metaData {
+            label(input: "metadata.labels.filename")
+            key(input: "original_filename")
+            __typename
+        }
+        contextMenuActions {
+            doLinkAction {
+                label(input: "contextMenu.contextMenuLinkAction.followLink")
+                icon(input: "AngleRight")
+                __typename
+            }
+            primaryMediafile: doGeneralAction {
+                label(
+                    input: "contextMenu.contextMenuGeneralAction.setPrimaryMediafile"
+            )
+                action(input: SetPrimaryMediafile)
+                icon(input: "Link")
+                __typename
+            }
+            primaryThumbnail: doGeneralAction {
+                label(
+                    input: "contextMenu.contextMenuGeneralAction.setPrimaryThumbnail"
+            )
+                action(input: SetPrimaryThumbnail)
+                icon(input: "ImageCheck")
+                __typename
+            }
+            deleteRelation: doElodyAction {
+                label(input: "contextMenu.contextMenuElodyAction.delete-relation")
+                action(input: DeleteRelation)
+                icon(input: "Trash")
+                __typename
+            }
+            deleteEntity: doElodyAction {
+                label(input: "contextMenu.contextMenuElodyAction.delete-entity")
+                action(input: DeleteEntity)
+                icon(input: "Trash")
+                __typename
+            }
+            __typename
+        }
+    }
+    allowedViewModes {
+        viewModes(
+            input: [{ viewMode: ViewModesList }, { viewMode: ViewModesGrid }]
+    ) {
+        ...viewModes
+        }
+    }
+    __typename
+}
+
+fragment taggableEntityConfiguration on TaggableEntityConfiguration {
+    taggableEntityType
+    createNewEntityFormQuery
+    relationType
+    metadataFilterForTagContent
+    replaceCharacterFromTagSettings {
+        replacementCharactersRegex
+        characterToReplaceWith
+    }
+    metadataKeysToSetAsAttribute
+    tag
+    tagConfigurationByEntity {
+        configurationEntityType
+        configurationEntityRelationType
+        tagMetadataKey
+        colorMetadataKey
+        metadataKeysToSetAsAttribute
+        secondaryAttributeToDetermineTagConfig
+    }
+}
+`
